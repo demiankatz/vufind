d20f7f07480ad9b33930a999613d92311ab72957
Padmasree Gade <pgade@villanova.edu>
2024-09-18 12:52:59 -0400

    stan and composer errors fixed

--- module/VuFind/src/VuFind/Db/Service/AuthHashService.php
+++ module/VuFind/src/VuFind/Db/Service/AuthHashService.php
@@ -30,10 +30,10 @@
 namespace VuFind\Db\Service;
 
 use DateTime;
-use VuFind\Db\Entity\AuthHashEntityInterface;
-use VuFind\Db\Entity\AuthHash;
-use VuFind\Log\LoggerAwareTrait;
 use Laminas\Log\LoggerAwareInterface;
+use VuFind\Db\Entity\AuthHash;
+use VuFind\Db\Entity\AuthHashEntityInterface;
+use VuFind\Log\LoggerAwareTrait;
 
 /**
  * Database service for auth_hash table.
@@ -130,7 +130,7 @@ class AuthHashService extends AbstractDbService implements
                 ->setCreated(new DateTime());
             $this->persistEntity($result);
         }
-        
+
         return $result;
     }
 
